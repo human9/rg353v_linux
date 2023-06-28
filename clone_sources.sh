4917fb6b4f324d6fa5dc3eb41738acd40c70a2df
jrs <salamonj9@gmail.com>
2023-06-28 12:35:30 +0930

    change sources config

--- clone_sources.sh
+++ clone_sources.sh
@@ -2,18 +2,18 @@
 
 set -x
 
-# Obtain linux-next
+# Obtain linux
 
-git clone --branch next-20230626 --depth 1 git://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git
+git clone --branch v6.5-rockchip-dts64-1 --depth 1 git://git.kernel.org/pub/scm/linux/kernel/git/mmind/linux-rockchip.git linux
 
 # Obtain u-boot
 
-git clone --branch v2023.07-rc5 --depth 1 git@source.denx.de:u-boot/u-boot.git
+git clone --branch v2023.07-rc4 --depth 1 https://source.denx.de/u-boot/u-boot.git u-boot
 
 # Obtain buildroot
 
-git clone --branch 2023.05 --depth 1 git://git.buildroot.net/buildroot
+git clone --branch 2023.05 --depth 1 git://git.buildroot.net/buildroot buildroot
 
 # Obtain rkbin
 
-git clone --depth 1 https://github.com/rockchip-linux/rkbin
+git clone --depth 1 https://github.com/rockchip-linux/rkbin rkbin
