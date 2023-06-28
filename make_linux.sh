d4052d58df05e55d90c31bb65557039d4b1c8992
jrs <salamonj9@gmail.com>
2023-06-28 12:36:07 +0930

    cleanup linux building

--- make_linux.sh
+++ make_linux.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-
-#cp next.config linux-next/.config 
-cd linux-next/
+cp rg353v.config linux/.config
+cd linux/
 export CROSS_COMPILE=aarch64-linux-gnu-
 export ARCH=arm64
-make menuconfig ARCH=arm64
+make menuconfig
 make -j8
-make dtbs ARCH=arm64
+make dtbs
 
