dc9417a8a0cf22a4f186aecd642bc3288590284b
jrs <salamonj9@gmail.com>
2023-07-02 17:41:18 +0930

    target 6.4

--- make_linux.sh
+++ make_linux.sh
@@ -2,9 +2,14 @@
 
 cp linux_new.config linux/.config
 cd linux/
-#export CFLAGS="-g0 -O2 march=armv8.2-a mtune=cortex-a55"
+export CFLAGS="-g0 -O2 march=armv8.2-a mtune=cortex-a55"
 export CROSS_COMPILE=aarch64-linux-gnu-
 export ARCH=arm64
+# get patchset
+mkdir -p patches
+b4 am -o patches 20230426143213.4178586-1-macroalpha82@gmail.com
+git am -i patches/*.mbx
+# make
 make menuconfig
 make -j8
 make Image.gz
