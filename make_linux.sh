42103e46ae1ee0e2e33a6f1d65573467f657fc2a
jrs <salamonj9@gmail.com>
2023-07-05 18:16:14 +0930

    want untainted kernel for a bit

--- make_linux.sh
+++ make_linux.sh
@@ -6,9 +6,12 @@ export CFLAGS="-g0 -O2 march=armv8.2-a mtune=cortex-a55"
 export CROSS_COMPILE=aarch64-linux-gnu-
 export ARCH=arm64
 # get patchset
-mkdir -p patches
-b4 am -o patches 20230426143213.4178586-1-macroalpha82@gmail.com
-git am -i patches/*.mbx
+#mkdir -p patches
+# this is needed for v2 displays
+#b4 am -o patches 20230426143213.4178586-1-macroalpha82@gmail.com
+# not actually needed, but this is the patch to add wifi support
+#b4 am -o patches 20230405200729.632435-10-martin.blumenstingl@googlemail.com
+#git am -i patches/*.mbx
 # make
 make menuconfig
 make -j8
